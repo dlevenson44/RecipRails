e832278de3e7d350115b724c7edef705bc7a1ff0
dlevenson44 <daniellevenson4@gmail.com>
2020-08-01 22:44:22 -0400

    Setup Nav bar, routes

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, Container } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { StoreProvider } from 'easy-peasy';
 import theme from '../styles/theme';
 import store from '../store/globalStore';
+import Nav from './Nav';
 import Home from './Home';
+import Login from './Login';
+import Register from './Register';
 
-const App = () => (
+
+const App = () => {
+	const state = store.getState();
+	return (
 		<StoreProvider store={store}>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
-				<BrowserRouter>
-					<Switch>
-						<Route exact path="/" component={() => <Home state={store} />} />
-					</Switch>
-				</BrowserRouter>
+				<Container>
+					<BrowserRouter>
+						<Switch>
+							<Nav links={state.links} isAuthenticated={state.session.isAuthenticated} />
+							<Route exact path="/" component={() => <Home state={state} />} />
+							<Route path="/login" component={() => <Login state={state} />} />
+							<Route path="/register" component={() => <Register state={state} />} />
+						</Switch>
+					</BrowserRouter>
+				</Container>
 			</ThemeProvider>
 		</StoreProvider>
-);
+	);
+};
 
 export default App;
---
+++ app/javascript/components/Nav.jsx
@@ -0,0 +1,25 @@
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import { Link as RouterLink } from 'react-router-dom';
+import { Link, Typography } from '@material-ui/core';
+
+const Nav = props => {
+	const { links, isAuthenticated } = props;
+	console.log('is authe?', isAuthenticated)
+	return (
+		<Typography>
+			<Link component={RouterLink} to="/">{links.constant}</Link>
+			{!isAuthenticated ? (
+				links.unauthenticated.map(item => (
+					<Link key={item} component={RouterLink} to={`/${item.toLowerCase()}`}>{item}</Link>
+				))
+			) : (
+				links.authenticated.map(item => (
+					<Link key={item} component={RouterLink} to={`/${item.toLowerCase()}`}>{item}</Link>
+				))
+			)}
+		</Typography>
+	);
+}
+
+export default Nav;
