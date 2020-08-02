c770c700cc6d59d60324ae5d1850d27afdb565fe
dlevenson44 <daniellevenson4@gmail.com>
2020-08-01 21:14:56 -0400

    Setup MUI and theme

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { StoreProvider } from 'easy-peasy';
+import theme from '../styles/theme';
 import store from '../store/globalStore';
 import Home from './Home';
 
 const App = () => (
 		<StoreProvider store={store}>
-			<BrowserRouter>
-				<Switch>
-					<Route exact path="/" component={() => <Home state={store} />} />
-				</Switch>
-			</BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<BrowserRouter>
+					<Switch>
+						<Route exact path="/" component={() => <Home state={store} />} />
+					</Switch>
+				</BrowserRouter>
+			</ThemeProvider>
 		</StoreProvider>
 );
 
