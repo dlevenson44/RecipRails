ba6d01b0bd79d89b65e19aa5af7a3135a0ce4796
dlevenson44 <daniellevenson4@gmail.com>
2020-08-21 17:33:44 -0400

    Move frontend routes to separate file

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,31 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider, Container } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { StoreProvider } from 'easy-peasy';
 import theme from '../styles/theme';
 import store from '../store/globalStore';
-import Nav from './Nav';
-import Home from './Home';
-import Login from './Authentication/Login';
-import Register from './Authentication/Register';
-
+import Routes from './Routes';
 
 const App = () => {
-	const state = store.getState();
 	return (
 		<StoreProvider store={store}>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
 				<Container>
-					<BrowserRouter>
-						<Nav links={state.links} isAuthenticated={state.user.isAuthenticated} />
-						<Switch>
-							<Route exact path="/" component={() => <Home state={state} />} />
-							<Route path="/login" component={() => <Login state={state} />} />
-							<Route path="/register" component={() => <Register state={state} />} />
-						</Switch>
-					</BrowserRouter>
+					<Routes />
 				</Container>
 			</ThemeProvider>
 		</StoreProvider>
---
+++ app/javascript/components/Routes.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { useStoreState } from 'easy-peasy';
+import Auth from './Authentication/authHelpers';
+import Nav from './Nav';
+import Home from './Home';
+import Login from './Authentication/Login';
+import Register from './Authentication/Register';
+
+const Routes = () => {
+	const state = useStoreState(state => state);
+	const { isAuthenticated } = state.user;
+	const authenticated = Auth.isUserAuthenticated() || isAuthenticated;
+
+	return (
+		<BrowserRouter>
+			<Nav links={state.links} isAuthenticated={authenticated} />
+			<Switch>
+				<Route exact path="/" component={() => <Home state={state} />} />
+				<Route path="/login" render={() => (!authenticated ? <Login state={state} /> : <Redirect to="/" /> )} />
+				<Route path="/register" render={() => (!authenticated ? <Register state={state} /> : <Redirect to="/" />)} />
+			</Switch>
+		</BrowserRouter>
+	);
+};
+
+export default Routes;
