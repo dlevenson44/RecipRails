b9c78460d2e754c50071a5cddc0186a62ecfc2d8
dlevenson44 <daniellevenson4@gmail.com>
2020-09-25 16:33:53 -0400

    Pass updated props to Recipe

--- app/javascript/components/RecipeSearch/SearchResults.jsx
+++ app/javascript/components/RecipeSearch/SearchResults.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import { useStoreState } from 'easy-peasy';
+import { useStoreActions, useStoreState } from 'easy-peasy';
 import Recipe from './Recipe';
+import Auth from '../Authentication/authHelpers';
 
 const SearchResults = () => {
-	const results = useStoreState(state => state.search.results);
+	const { isAuthenticated } = useStoreState(state => state.user)
+	const { results } = useStoreState(state => state.search);
+	const { add } = useStoreActions(actions => actions.favorites)
+	const token = Auth.getToken()
+	const handleClick = recipe => {
+		console.log('adding recipe:   ', recipe)
+		return add({ ...recipe, token })
+	}
 
 	if (!!results.length) {
-		return results.map((recipe, idx) => (
-			<Recipe recipe={recipe.recipe} key={idx} />
+		return results.map(({
+			calories,
+			label,
+			ingredientLines,
+			shareAs
+		}, idx) => (
+			<Recipe
+				label={label}
+				calories={calories}
+				ingredients={shareAs}
+				instructions={ingredientLines}
+				key={idx}
+				isAuthenticated={isAuthenticated}
+				onClick={handleClick}
+			/>
 		));
 	};
 
